ddff7237917238edca72185fdb1ecf30044df13c
Marco Hartmann <marcohartmann95@gmail.com>
2023-09-23 21:04:50 +0200

    Live feed using opencv

--- wifi_microscope_dump.py
+++ wifi_microscope_dump.py
@@ -1,8 +1,8 @@
-
 # Proof-of-concept code for reading data from a Wifi microscope.
 # See https://www.chzsoft.de/site/hardware/reverse-engineering-a-wifi-microscope/.
 
 # Copyright (c) 2020, Christian Zietz <czietz@gmx.net>
+# Copyright (c) 2023, mhrtmnn
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -24,50 +24,79 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import time
 import socket
-import msvcrt
+import numpy as np
+import cv2
+import signal
+import sys
 
 HOST = "192.168.29.1"	# Microscope hard-wired IP address
 SPORT = 20000			# Microscope command port
 RPORT = 10900			# Receive port for JPEG frames
-o = None
 
-# Open command socket for sending
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-	# s.sendto(b"JHCMD\xd0\x00", (HOST, SPORT))
-	# Send commands like naInit_Re() would do
-	s.sendto(b"JHCMD\x10\x00", (HOST, SPORT))
-	s.sendto(b"JHCMD\x20\x00", (HOST, SPORT))
-	# Heartbeat command, starts the transmission of data from the scope
-	s.sendto(b"JHCMD\xd0\x01", (HOST, SPORT))
+def heartbeat(s):
 	s.sendto(b"JHCMD\xd0\x01", (HOST, SPORT))
 
-	# Open receive socket and bind to receive port
-	with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as r:
-		r.bind(("", RPORT))
-		r.setblocking(0)
-		# Note: Modify this line if not running under Windows
-		while not msvcrt.kbhit():
-			try:
-				data = r.recv(1450)
+def display_frame(buf):
+	arr = np.frombuffer(buf, dtype="uint8")
+	img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+	# print(f"Decoded image: {img.shape}")
+	cv2.namedWindow("wifi microscope", cv2.WINDOW_NORMAL)
+	cv2.imshow("wifi microscope", img)
+	cv2.waitKey(5)
+
+def main():
+	# Open command socket for sending
+	with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sTx:
+		# sTx.sendto(b"JHCMD\xd0\x00", (HOST, SPORT))
+		# Send commands like naInit_Re() would do
+		sTx.sendto(b"JHCMD\x10\x00", (HOST, SPORT))
+		sTx.sendto(b"JHCMD\x20\x00", (HOST, SPORT))
+
+		# Heartbeat command, starts the transmission of data from the scope
+		heartbeat(sTx)
+		heartbeat(sTx)
+
+		imgBuf = bytearray()
+		skipFirst = True
+
+		# Open receive socket and bind to receive port
+		with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sRx:
+			sRx.bind(("", RPORT))
+			sRx.settimeout(5.0)
+
+			# clean exit
+			def signal_handler(sig, frame):
+				print('Closing stream ...')
+				# Stop data command, like in naStop()
+				sTx.sendto(b"JHCMD\xd0\x02", (HOST, SPORT))
+				sRx.close()
+				sTx.close()
+				sys.exit(0)
+			signal.signal(signal.SIGINT, signal_handler)
+
+			print("Starting stream")
+			while True:
+				data = sRx.recv(1450)
 				if len(data) > 8:
 					# Header
-					framecount = data[0] + data[1]*256
-					packetcount = data[3]
-					print("Frame %d, packet %d" % (framecount, packetcount))
+					frameCount = data[0] + data[1]*256
+					packetCount = data[3]
+
 					# Data
-					if packetcount==0:
-						# A new frame has started, open a new file
-						if o is not None:
-							o.close()
-						# Only save 100 frames to avoid filling up the disk
-						o = open("frame_%d.jpg" % (framecount%100), "wb")
+					if packetCount==0:
+						# A new frame has started
+						# print(f"Frame {frameCount}")
+						if not skipFirst:
+							display_frame(imgBuf)
+						skipFirst = False
+						imgBuf = bytearray()
+
 						# Send a heartbeat every 50 frames (arbitrary number) to keep data flowing
-						if framecount%50 == 0:
-							s.sendto(b"JHCMD\xd0\x01", (HOST, SPORT))
-					o.write(data[8:])
-			except:
-				time.sleep(0.01)
-	# Stop data command, like in naStop()
-	s.sendto(b"JHCMD\xd0\x02", (HOST, SPORT))
+						if frameCount % 50 == 0:
+							heartbeat(sTx)
+
+					imgBuf += data[8:]
+
+if __name__ == "__main__":
+	main()
